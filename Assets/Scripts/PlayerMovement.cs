db8b505579cc92991edcb02e4392192f9b88c6a1
drago314 <avrickaltmann@yahoo.com>
2021-06-20 21:54:15 -0700

    Add animations for player
    Jump, walk and idle animations added, as well as infinite jumping bug fixed.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float jumpForce;
     private Rigidbody2D body;
     private Animator anim;
+    private bool grounded;
 
     private void Awake() {
 
@@ -22,20 +23,29 @@ public class PlayerMovement : MonoBehaviour
 
         //Flipping Character Model
         if (horizontalInput > 0.01f)
-        {
             transform.localScale = new Vector2((float) 0.75, (float) 0.75);
-        }
         else if (horizontalInput < -0.01f)
-        {
             transform.localScale = new Vector2((float) -0.75, (float) 0.75);
-        }
 
         //Jumping
-        if(Input.GetKey(KeyCode.Space))
-        {
-            body.velocity = new Vector2(body.velocity.x, jumpForce);
-        }
+        if(Input.GetKey(KeyCode.Space) && grounded)
+            Jump();
 
+        //Set animation parameters
         anim.SetBool("Run", horizontalInput != 0);
+        anim.SetBool("Grounded", grounded);
     }   
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, jumpForce);
+        grounded = false;
+        anim.SetTrigger("Jump");
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) 
+    {
+        if (other.gameObject.tag == "Ground")
+            grounded = true;
+    }
 }
