86ed71a0b2f9b8854563fe75d9e8b1289b30ae4f
drago314 <avrickaltmann@yahoo.com>
2021-06-21 17:56:07 -0700

    Add "better-jump"
    Better jump makes jumps larger when space button held and shorter when held shorter periods of time.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,48 +4,94 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private float jumpForce;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask wallLayer;
+    [SerializeField] private float fallMultiplier;
+    [SerializeField] private float lowJumpMultiplier; 
+    [SerializeField] private float wallSlidingSpeed;
+    private BoxCollider2D boxCollider;
     private Rigidbody2D body;
     private Animator anim;
     private bool grounded;
+    private bool onWall;
+    private float horizontalInput;
 
-    private void Awake() {
+
+    private void Awake()
+    {
 
         //Grab references 
+        boxCollider = GetComponent<BoxCollider2D>();
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
     }
 
-    private void Update() {
-        float horizontalInput = Input.GetAxis("Horizontal");
-
-        //Moving Character Left/Right
-        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+    private void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
 
         //Flipping Character Model
         if (horizontalInput > 0.01f)
-            transform.localScale = new Vector2((float) 0.75, (float) 0.75);
+            transform.localScale = new Vector2(1, 1);
         else if (horizontalInput < -0.01f)
-            transform.localScale = new Vector2((float) -0.75, (float) 0.75);
-
-        //Jumping
-        if(Input.GetKey(KeyCode.Space) && grounded)
-            Jump();
+            transform.localScale = new Vector2(-1, 1);
 
         //Set animation parameters
         anim.SetBool("Run", horizontalInput != 0);
         anim.SetBool("Grounded", grounded);
-    }   
+
+        Move();
+        CheckIfGrounded();
+        CheckIfOnWall();
+        Jump();
+        BetterJump();
+        WallSlide();
+    }
+
+    private void Move()
+    {
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+    }
 
     private void Jump()
     {
-        body.velocity = new Vector2(body.velocity.x, jumpForce);
-        grounded = false;
-        anim.SetTrigger("Jump");
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            body.velocity = new Vector2(body.velocity.x, jumpForce);
+            anim.SetTrigger("Jump");
+        }
     }
 
-    private void OnCollisionEnter2D(Collision2D other) 
+    void BetterJump()
     {
-        if (other.gameObject.tag == "Ground")
-            grounded = true;
+        if (body.velocity.y < 0)
+        {
+            body.velocity += Vector2.up * Physics2D.gravity * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (body.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            body.velocity += Vector2.up * Physics2D.gravity * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+
+    private void WallSlide()
+    {
+        if (onWall && !grounded && horizontalInput != 0)
+        {
+            body.velocity = new Vector2(body.velocity.x, Mathf.Clamp(body.velocity.y, -wallSlidingSpeed, -float.MaxValue));
+        }
+    }
+    private void CheckIfOnWall()
+    {
+        RaycastHit2D raycastHitRight = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.right, 0.1f, groundLayer);
+        RaycastHit2D raycastHitLeft = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.left, 0.1f, groundLayer);
+        onWall = raycastHitLeft.collider != null || raycastHitRight.collider != null;
+    }
+
+    private void CheckIfGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.05f, groundLayer);
+        grounded = raycastHit.collider != null;
     }
 }
+
