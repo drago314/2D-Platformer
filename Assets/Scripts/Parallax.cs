b73add272419d5dcfebdb4d6f8f0279ac6a515a5
drago314 <avrickaltmann@yahoo.com>
2021-06-23 14:18:58 -0700

    Add foreground layer

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -34,7 +34,7 @@ public class Parallax : MonoBehaviour
             float parallax = (previousCamPosition.x - cam.position.x) * parallaxScales[i];
             float backgroundTargetPosX = backgrounds[i].position.x + parallax;
             Vector3 backgroundTargetPosition = new Vector3(backgroundTargetPosX, backgrounds[i].position.y, backgrounds[i].position.z);
-            backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, backgroundTargetPosition, Time.deltaTime);
+            backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, backgroundTargetPosition, Time.deltaTime * smoothing);
         }
 
         previousCamPosition = cam.position;
