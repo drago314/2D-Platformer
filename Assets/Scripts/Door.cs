0766c63d66c32796ffd8d70d984613075d7f4350
drago314 <avrickaltmann@yahoo.com>
2021-06-24 21:27:00 -0700

    Add Room Transition

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] private string sceneName;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+
+    }
+}
