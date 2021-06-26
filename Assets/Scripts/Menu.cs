e6feb6a2f55cfd06dc96a0ddeb05eebb99859529
drago314 <avrickaltmann@yahoo.com>
2021-06-25 22:00:33 -0700

    Fix Level Transition Rendering Under Health

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -5,9 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    [SerializeField] LevelLoader levelLoader;
+
     public void PlayGame()
     {
-        SceneManager.LoadScene("Level 1");
+        levelLoader.LoadScene("Level 1");
     }
 
     public void QuitGame()
